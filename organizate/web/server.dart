6cd93cc5b70cdae83f672af0e0040fb6dd09de86
hombrehaciatras <byfnk@correo.ugr.es>
2014-03-20 16:35:19 +0100

    Fran

--- organizate/web/server.dart
+++ organizate/web/server.dart
@@ -9,26 +9,30 @@ const String webHost = "127.0.0.1";
 const int webPort = 8888;
 const String SQLuser = "root";
 const String SQLpassword = "MbP2FrrWdvsu";
-const int SQLport = 3306;
+const int SQLport = 37184;
 const String SQLdb = "Calendar";
 const String SQLhost = "91.121.7.186";
 ConnectionPool Conexion;
-
+Map datos;
 
 void main(){
   
   HttpServer.bind(webHost, webPort).then((server) {
       server.listen((HttpRequest request) {
-        switch(request.uri.path){
-            case "/submit":
-              //JsonObject data = new JsonObject.fromJsonString(request.responseText);
-              print("recibido");
-              break;
-            default:
-              break;
-        }
-            
+        request.listen((List<int> buffer){
+          var jsonString = new String.fromCharCodes(buffer);
+          datos = JSON.decode(jsonString);
         });
+
+        switch(request.uri.path){
+              case "/submit":
+                registrar(datos);
+                datos = {};
+                break;
+              default:
+                break;
+        }
+      });
       });
 }
 
@@ -38,19 +42,22 @@ void serverHandler(HttpRequest request){
   
 }
 
-void registrar(String correo, String nombre, String contrasena, int age){
+void registrar(Map datos){
   
     Conexion = new ConnectionPool(host: SQLhost, port: SQLport, user: SQLuser, password: SQLpassword, db: SQLdb);
     Conexion.prepare(
-        'insert into Usuarios (Correo, Nombre, Contrasena) values (?, ?, ?)'
+        'insert into Usuarios (Correo, Nombre, Apellido, Contrasena, FechaNacimiento,) values (?, ?, ?, ?, ?)'
       ).then((query) {
         print('prepared query insert into task');
         var params = new List();
-          params.add(correo);
-          params.add(nombre);
-          params.add(contrasena);
+          params.add(datos["correo"]);
+          params.add(datos["nombre"]);
+          params.add(datos["apellido"]);
+          params.add(datos["contrasena"]);
+          params.add(datos["nacimiento"]);
         return query.execute(params);
       });  
+    
     Conexion.close();
 }
 
